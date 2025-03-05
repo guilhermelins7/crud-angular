1f03c888deedb4d135d4192cc4014c0f1e13a7a5
guilhermelins7 <guilherme7lins@gmail.com>
2025-03-05 17:45:55 -0300

    feat: adicionar visualização dinamica do card com ngModel TWDB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    CriarPensamentoComponent
+    CriarPensamentoComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  criarPensamento() {
+    alert('novo pensamento criado!');
   }
 
+  cancelar() {
+    alert('criação de pensamento cancelado!');
+  }
+
+  ngOnInit(): void {}
 }
